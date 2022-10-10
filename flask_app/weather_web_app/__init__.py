1b607589ba3bbea5ed319c476cfa2946a2a9d9e2
Kyle Ausen <ausenkyle@gmail.com>
2022-10-10 10:08:40 -0600

    initial commit.

--- flask_app/weather_web_app/__init__.py
+++ flask_app/weather_web_app/__init__.py
@@ -9,3 +9,18 @@ def create_app(test_config=None):
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'weather_web_app.sqlite')
     )
+
+    if test_config is None:
+        # Load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # Load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # Ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    return app
